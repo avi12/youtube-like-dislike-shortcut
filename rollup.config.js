4a3863811c2caf3f12913d02a8ea55aa6546527f
avi12 <avi6106@gmail.com>
2021-05-06 22:51:09 +0300

    Rollup: Removed "buttons" script; changed format to "iife"

--- rollup.config.js
+++ rollup.config.js
@@ -6,17 +6,13 @@ function createConfig(filename) {
   return {
     input: `src/scripts/${filename}.js`,
     output: {
-      format: "esm",
+      format: "iife",
       file: `dist/build/${filename}.js`
     },
-    plugins: [isProduction && terser()],
-    watch: {
-      clearScreen: false
-    }
+    plugins: [isProduction && terser()]
   };
 }
 
 export default [
-  createConfig("content-script-youtube-rate-buttons"),
   createConfig("content-script-youtube-rate-trigger")
 ];
