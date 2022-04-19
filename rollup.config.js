41783d412d9ca5f8b034e789132ffcbb1025c878
avi12 <avi6106@gmail.com>
2022-04-19 21:27:19 +0300

    Updated Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,48 @@
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import svelte from "rollup-plugin-svelte";
 import { terser } from "rollup-plugin-terser";
+import css from "rollup-plugin-css-only";
 import typescript from "@rollup/plugin-typescript";
+import sveltePreprocess from "svelte-preprocess";
 
 const isProduction = !process.env.ROLLUP_WATCH;
 
-function createConfig(filename) {
+function createConfig(filename, useSvelte = false) {
   return {
-    input: `src/scripts/${filename}.ts`,
+    input: `src/${filename}.ts`,
     output: {
       format: "cjs",
-      file: `dist/build/${filename}.js`
+      file: `dist/build/${filename}.js`,
+      sourcemap: false,
+      inlineDynamicImports: true
+    },
+    plugins: [
+      typescript(),
+      useSvelte && css({ output: "popup.css" }),
+      useSvelte &&
+        svelte({
+          compilerOptions: {
+            dev: !isProduction
+          },
+          preprocess: sveltePreprocess()
+        }),
+      resolve({ dedupe: ["svelte"] }),
+      commonjs(),
+      isProduction && terser()
+    ],
+    watch: {
+      clearScreen: true
+    }
+  };
+}
+
+function createBackgroundConfig() {
+  return {
+    input: "src/scripts/background.ts",
+    output: {
+      format: "cjs",
+      file: "dist/build/scripts/background.js"
     },
     plugins: [typescript(), isProduction && terser()],
     watch: {
@@ -17,4 +51,8 @@ function createConfig(filename) {
   };
 }
 
-export default [createConfig("content-script-youtube-rate-trigger")];
+export default [
+  createConfig("scripts/content-script-youtube-rate-trigger"),
+  createConfig("popup/popup", true),
+  createBackgroundConfig()
+];
