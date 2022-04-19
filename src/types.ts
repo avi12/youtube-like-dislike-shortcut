0342e47bdbbf6e094798be621a7d3c401ff3ccc3
avi12 <avi6106@gmail.com>
2022-04-19 22:09:58 +0300

    Added types

---
+++ src/types.ts
@@ -0,0 +1,16 @@
+export type Modifier = "shiftKey" | "ctrlKey" | "altKey" | "metaKey";
+
+export interface ButtonTrigger {
+  primary: string;
+  modifiers: Modifier[];
+  secondary: string;
+}
+
+export interface ButtonTriggers {
+  like: ButtonTrigger;
+  dislike: ButtonTrigger;
+  unrate: ButtonTrigger;
+}
+
+export type SupportedActions = "like" | "dislike" | "unrate";
+export type RecordingType = SupportedActions | null;
