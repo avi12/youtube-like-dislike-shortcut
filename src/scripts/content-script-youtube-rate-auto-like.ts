844f2604d0b250578802c4121dbb89fe5e2e1416
avi12 <avi6106@gmail.com>
2022-06-20 18:28:17 +0300

    content-script-youtube-rate-auto-like.ts: - Optimized code further - Dealing with edge-cases regarding listening to moving between pages

--- src/scripts/content-script-youtube-rate-auto-like.ts
+++ src/scripts/content-script-youtube-rate-auto-like.ts
@@ -1,17 +1,35 @@
-import { getActiveButton, rateVideo } from "./content-script-youtube-rate-buttons";
-import { getVisibleElement, Selectors } from "../utils-initials";
+import { getActiveButton, getRateButtons, rateVideo } from "./content-script-youtube-rate-buttons";
+import { getIsElementVisible, getVisibleElement, Selectors } from "../utils-initials";
 
 const observerOptions: MutationObserverInit = { childList: true, subtree: true };
 let gTitleLast = document.title;
-let gPlayerObserver: MutationObserver;
+let gUrlLast = location.href;
+
+const gPlayerObserver = new MutationObserver((_, observer) => {
+  const elVideo = getVisibleElement<HTMLVideoElement>(Selectors.video);
+  const elLiveBadge = document.querySelector<HTMLDivElement>(Selectors.live);
+  const [elLike] = getRateButtons();
+  if (!elVideo || !elLike || !elLiveBadge) {
+    return;
+  }
+
+  observer.disconnect();
+
+  const isRated = getActiveButton();
+  const isLiveOrPremiere = getIsElementVisible(elLiveBadge);
+  if (isRated || isLiveOrPremiere) {
+    return;
+  }
+
+  gTimeCurrentLast = elVideo.currentTime;
+  stopTracking(elVideo);
+  startTracking(elVideo);
+});
+
 let gTimeCounter = 0;
 let gTimeDelta: number;
 let gTimeCurrentLast: number;
 const REGEX_SUPPORTED_PAGES = /^\/(?:watch|shorts)/;
-
-function getIsLive(): HTMLElement {
-  return getVisibleElement(Selectors.live);
-}
 
 function startTracking(elVideo: HTMLVideoElement): void {
   elVideo.addEventListener("timeupdate", autoLikeWhenNeeded);
@@ -27,7 +45,6 @@ export function getIsPassedThreshold(): boolean {
 
 function autoLikeWhenNeeded(e: Event): void {
   const elVideo = <HTMLVideoElement>e.target;
-  window.ytrUserInteracted = window.ytrUserInteracted || Boolean(getActiveButton());
   if (window.ytrUserInteracted) {
     stopTracking(elVideo);
     return;
@@ -71,17 +88,19 @@ export function setPercentageWatched({
   percentage: number;
   isVisible: boolean;
 }): void {
-  const { parentElement: elContainer } = getVisibleElement(Selectors.toggleButtonsNormalVideo);
+  const { percentageWatched, toggleButtonsNormalVideo } = Selectors;
+  const container = getVisibleElement(`${toggleButtonsNormalVideo}:not(${percentageWatched})`);
+  const { parentElement: elContainer } = container;
   if (!elContainer) {
     return;
   }
 
-  const elIndicatorFound = elContainer.querySelector(Selectors.percentageWatched);
+  const elIndicatorFound = elContainer.querySelector(percentageWatched);
   const elIndicator = elIndicatorFound || document.createElement("ytd-toggle-button-renderer");
 
-  const classNames = [Selectors.percentageWatched.substring(1), !isVisible ? "hidden" : ""];
+  const classNames = [percentageWatched.substring(1), !isVisible ? "hidden" : ""];
   elIndicator.className = classNames.join(" ");
-  elIndicator.innerHTML = `${percentage.toFixed(2)}%`;
+  elIndicator.textContent = `${percentage.toFixed(2)}%`;
 
   if (!elIndicatorFound) {
     elContainer.prepend(elIndicator);
@@ -89,34 +108,19 @@ export function setPercentageWatched({
 }
 
 function addTemporaryBodyListener(): void {
-  // For some reason, the title observer will run as soon as .observer() calls,
-  // so we need to prevent it
-  if (gTitleLast === document.title) {
+  if (gTitleLast === document.title || gUrlLast === location.href) {
     return;
   }
 
-  // Runs only on /watch & /shorts pages where the user has not rated, nor it is a livestream
-  if (!location.pathname.match(REGEX_SUPPORTED_PAGES) || getActiveButton() || getIsLive()) {
+  // Runs only on /watch & /shorts pages
+  if (!location.pathname.match(REGEX_SUPPORTED_PAGES)) {
     return;
   }
 
   gTitleLast = document.title;
+  gUrlLast = location.href;
   window.ytrUserInteracted = false;
   gTimeCounter = 0;
-
-  if (!gPlayerObserver) {
-    gPlayerObserver = new MutationObserver(() => {
-      const elVideo = getVisibleElement<HTMLVideoElement>(Selectors.video);
-      if (!elVideo) {
-        return;
-      }
-
-      gPlayerObserver.disconnect();
-      gTimeCurrentLast = elVideo.currentTime;
-      stopTracking(elVideo);
-      startTracking(elVideo);
-    });
-  }
 
   gPlayerObserver.observe(document, observerOptions);
 }
@@ -129,18 +133,8 @@ function addGlobalEventListenerOnDesktop(): void {
 export function prepareToAutoLike(): void {
   addGlobalEventListenerOnDesktop();
 
-  // Runs only on /watch & /shorts pages where the user has not rated, nor it is a livestream
-  new MutationObserver((_, observer) => {
-    if (!location.pathname.match(REGEX_SUPPORTED_PAGES) || getActiveButton() || getIsLive()) {
-      observer.disconnect();
-      return;
-    }
-
-    const elVideo = getVisibleElement<HTMLVideoElement>(Selectors.video);
-    if (elVideo) {
-      gTimeCurrentLast = elVideo.currentTime;
-      observer.disconnect();
-      startTracking(elVideo);
-    }
-  }).observe(document, observerOptions);
+  // Runs only on /watch & /shorts pages
+  if (location.pathname.match(REGEX_SUPPORTED_PAGES)) {
+    gPlayerObserver.observe(document, observerOptions);
+  }
 }
