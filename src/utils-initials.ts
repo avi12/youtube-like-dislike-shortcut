08f5a2f017ddd46191e14a20ab396fb4ac25efc8
avi12 <avi6106@gmail.com>
2023-12-11 01:22:38 +0200

    utils-initials.ts: Improved Like-Dislike button selector

--- src/utils-initials.ts
+++ src/utils-initials.ts
@@ -33,7 +33,7 @@ export enum SELECTORS {
   adSkipNow = ".ytp-ad-skip-button-text",
   liveBadge = ".ytp-live-badge, .ytp-offline-slate-bar",
   percentageWatched = ".ytr-percentage",
-  toggleButtonsNormalVideo = "[class*=YtSegmentedLikeDislikeButton] yt-smartimation, ytd-segmented-like-dislike-button-renderer yt-smartimation",
+  toggleButtonsNormalVideo = "#top-level-buttons-computed yt-smartimation, ytd-segmented-like-dislike-button-renderer yt-smartimation",
   toggleButtonsShortsVideo = "ytd-like-button-renderer",
   buttonSubscribe = "ytd-subscribe-button-renderer",
   // Bezel classes
